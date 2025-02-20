a0e2c344386eecd09e11d3d636594e68a7643f27
Sheriff Adio <flamezee15@gmail.com>
2025-02-20 18:27:22 +0100

    suscription model

--- models/user.model.js
+++ models/user.model.js
@@ -19,8 +19,7 @@ const userSchema = new mongoose.Schema({
         maxLength: 100,
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email',
-        ],
+            'Please provide a valid email'],
         
     },
     password: {

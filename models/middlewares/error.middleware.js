8e46c17ed9d4018e5a4968da3d4409a8950dc823
Sheriff Adio <flamezee15@gmail.com>
2025-02-24 17:31:14 +0100

    end of middleWare

--- models/middlewares/error.middleware.js
+++ models/middlewares/error.middleware.js
@@ -13,12 +13,25 @@ if (err.name === 'CastError') {
 }
 
 // Mongoose duplicate key
-if (err.name === 'validationError') {
-    const message = Object.values(err.errors).map(val => val.message);
+if (err.code === '11000') {
+    const message = 'Duplicate field value entered';
     error = new Error(message);
-    error.status = 400;
-}
+    error.statusCode = 400;
+    }
+    
+ // Mongoose validation error
+     if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+         error = new Error(message.join('. '));
+         error.statusCode = 400;
+        }
+    res.status(error.statusCode || 500).json({
+        success: false,
+        message: error.message || 'Server Error',
+    })
     } catch (error) {
         next(error);
     }
 };
+
+export default errorMiddleware;
